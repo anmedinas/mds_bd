8edf1a9c9f298a55da5c18eb26e94e649e236d55
aplandaeta <antonia.landaeta@ug.uchile.cl>
2025-06-09 17:38:43 -0400

    Update create_table_bureau.sql

--- query/create_table_bureau.sql
+++ query/create_table_bureau.sql
@@ -2,27 +2,28 @@
 
 DROP TABLE IF EXISTS bureau;
 
-CREATE TABLE bureau (
-    SK_ID_CURR INT, -- ID del préstamo en nuestra muestra (puede tener 0, 1, 2 o más créditos relacionados en el Bureau)
-    SK_BUREAU_ID INT, -- ID único del crédito en el Bureau relacionado con el préstamo en Home Credit
-    CREDIT_ACTIVE VARCHAR(50), -- Estado del crédito en el Bureau (por ejemplo, 'Active', 'Closed', etc.)
-    CREDIT_CURRENCY VARCHAR(10), -- Moneda del crédito en el Bureau
-    DAYS_CREDIT INT, -- Días antes de la solicitud actual en que el cliente solicitó el crédito en el Bureau
-    CREDIT_DAY_OVERDUE INT, -- Número de días de retraso en el crédito del Bureau en el momento de la solicitud
-    DAYS_CREDIT_ENDDATE INT, -- Duración restante del crédito en el Bureau (en días) en el momento de la solicitud
-    DAYS_ENDDATE_FACT INT, -- Días desde que el crédito del Bureau terminó en el momento de la solicitud (solo para créditos cerrados)
-    AMT_CREDIT_MAX_OVERDUE DECIMAL(15, 2), -- Monto máximo vencido en el crédito del Bureau hasta la fecha de la solicitud
-    CNT_CREDIT_PROLONG INT, -- Número de veces que se prolongó el crédito del Bureau
-    AMT_CREDIT_SUM DECIMAL(15, 2), -- Monto actual del crédito en el Bureau
-    AMT_CREDIT_SUM_DEBT DECIMAL(15, 2), -- Deuda actual en el crédito del Bureau
-    AMT_CREDIT_SUM_LIMIT DECIMAL(15, 2), -- Límite de crédito actual de la tarjeta de crédito reportada en el Bureau
-    AMT_CREDIT_SUM_OVERDUE DECIMAL(15, 2), -- Monto actual vencido en el crédito del Bureau
-    CREDIT_TYPE VARCHAR(50), -- Tipo de crédito en el Bureau (por ejemplo, 'Car', 'Cash', etc.)
-    DAYS_CREDIT_UPDATE INT, -- Días antes de la solicitud en que se actualizó la última información sobre el crédito del Bureau
-    AMT_ANNUITY DECIMAL(15, 2) -- Anualidad del crédito en el Bureau
+CREATE TABLE IF NOT EXISTS bureau
+(
+    sk_id_curr integer NOT NULL,
+    sk_bureau_id integer NOT NULL,
+    credit_active character varying(50) COLLATE pg_catalog."default",
+    credit_currency character varying(10) COLLATE pg_catalog."default",
+    days_credit integer,
+    credit_day_overdue integer,
+    days_credit_enddate double precision,
+    days_enddate_fact double precision,
+    amt_credit_max_overdue numeric(15,2),
+    cnt_credit_prolong integer,
+    amt_credit_sum numeric(15,2),
+    amt_credit_sum_debt numeric(15,2),
+    amt_credit_sum_limit numeric(15,2),
+    amt_credit_sum_overdue numeric(15,2),
+    credit_type character varying(50) COLLATE pg_catalog."default",
+    days_credit_update double precision,
+    amt_annuity numeric(15,2),
     CONSTRAINT pk_bureau PRIMARY KEY (sk_id_curr, sk_bureau_id)
+)
 
-);
 
 SET ROLE postgres;
 COPY bureau
