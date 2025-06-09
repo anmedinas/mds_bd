112d9171ee5140a1d951b092a065b4bfeee2b815
aplandaeta <antonia.landaeta@ug.uchile.cl>
2025-06-09 17:39:05 -0400

    Update create_table_bureau_balance.sql

--- query/create_table_bureau_balance.sql
+++ query/create_table_bureau_balance.sql
@@ -1,12 +1,13 @@
 DROP TABLE IF EXISTS bureau_balance;
 
-CREATE TABLE bureau_balance (
-    SK_BUREAU_ID INT, -- ID único del crédito en el Bureau relacionado con el préstamo en Home Credit
-    MONTHS_BALANCE INT, -- Mes relativo al balance (0 = mes actual, valores negativos = meses anteriores)
-    CREDIT_STATUS CHAR(1) CHECK (CREDIT_STATUS IN ('C', 'X', '0', '1', '2', '3', '4', '5')) -- Estado del crédito durante el mes
+CREATE TABLE IF NOT EXISTS proyecto.bureau_balance
+(
+    sk_bureau_id integer NOT NULL,
+    months_balance integer NOT NULL,
+    credit_status character(1) COLLATE pg_catalog."default",
     CONSTRAINT pk_bureau_balance PRIMARY KEY (sk_bureau_id, months_balance),
     CONSTRAINT bureau_balance_credit_status_check CHECK (credit_status = ANY (ARRAY['C'::bpchar, 'X'::bpchar, '0'::bpchar, '1'::bpchar, '2'::bpchar, '3'::bpchar, '4'::bpchar, '5'::bpchar]))
-);
+)
 
 SET ROLE postgres;
 COPY bureau_balance
