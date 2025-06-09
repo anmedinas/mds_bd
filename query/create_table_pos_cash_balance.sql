17746bf4285969382217a18a4efcda05e4fbcbee
aplandaeta <antonia.landaeta@ug.uchile.cl>
2025-06-09 17:40:11 -0400

    Update create_table_pos_cash_balance.sql

--- query/create_table_pos_cash_balance.sql
+++ query/create_table_pos_cash_balance.sql
@@ -1,18 +1,18 @@
 -- Crear la tabla pos_cash_balance
 DROP TABLE IF EXISTS pos_cash_balance; 
 
-CREATE TABLE pos_cash_balance (
-    SK_ID_PREV INT, -- ID único del crédito previo en Home Credit
-    SK_ID_CURR INT, -- ID único del cliente
-    MONTHS_BALANCE INT, -- Mes relativo al balance (0 = mes actual, valores negativos = meses anteriores)
-    CNT_INSTALMENT INT, -- Número total de cuotas del crédito previo
-    CNT_INSTALMENT_FUTURE INT, -- Número de cuotas restantes por pagar en el crédito previo
-    NAME_CONTRACT_STATUS VARCHAR(50), -- Estado del contrato durante el mes (por ejemplo, 'Active', 'Completed')
-    SK_DPD INT, -- Días de retraso en el pago durante el mes
-    SK_DPD_DEF INT -- Días de retraso en el pago con tolerancia durante el mes
+CREATE TABLE IF NOT EXISTS pos_cash_balance
+(
+    sk_id_prev integer NOT NULL,
+    sk_id_curr integer NOT NULL,
+    months_balance integer NOT NULL,
+    cnt_instalment double precision,
+    cnt_instalment_future double precision,
+    name_contract_status character varying(50) COLLATE pg_catalog."default",
+    sk_dpd integer,
+    sk_dpd_def integer,
     CONSTRAINT pk_pc_balance PRIMARY KEY (sk_id_prev, sk_id_curr, months_balance)
-
-);
+)
 
 SET ROLE postgres;
 COPY pos_cash_balance
