a0af3160030606703d0e8b9ba6ba4b7c88caf31c
aplandaeta <antonia.landaeta@ug.uchile.cl>
2025-06-09 17:39:49 -0400

    Update create_table_installments_payments.sql

--- query/create_table_installments_payments.sql
+++ query/create_table_installments_payments.sql
@@ -2,18 +2,19 @@
 
 DROP TABLE IF EXISTS installments_payments ; 
 
-CREATE TABLE installments_payments (
-    SK_ID_PREV INT, -- ID único del crédito previo en Home Credit
-    SK_ID_CURR INT, -- ID único del cliente
-    NUM_INSTALMENT_VERSION INT, -- Versión del calendario de cuotas (0 para tarjetas de crédito)
-    NUM_INSTALMENT_NUMBER INT, -- Número de la cuota observada
-    DAYS_INSTALMENT INT, -- Días en los que se debía pagar la cuota (relativo a la fecha de la solicitud actual)
-    DAYS_ENTRY_PAYMENT INT, -- Días en los que se pagó realmente la cuota (relativo a la fecha de la solicitud actual)
-    AMT_INSTALMENT DECIMAL(15, 2), -- Monto prescrito de la cuota
-    AMT_PAYMENT DECIMAL(15, 2) -- Monto realmente pagado por el cliente
+CREATE TABLE IF NOT EXISTS proyecto.installments_payments
+(
+    sk_id_prev integer,
+    sk_id_curr integer,
+    num_instalment_version double precision,
+    num_instalment_number double precision,
+    days_instalment double precision,
+    days_entry_payment double precision,
+    amt_instalment numeric(15,2),
+    amt_payment numeric(15,2),
     id integer NOT NULL DEFAULT nextval('proyecto.installments_payments_id_seq'::regclass),
-    CONSTRAINT installments_payments_pkey PRIMARY KEY (id) -- llave artificial pq no habia combinacion unica de atributos sin tener que considerarlos todos como llave
-);
+    CONSTRAINT installments_payments_pkey PRIMARY KEY (id)
+)
 
 SET ROLE postgres;
 COPY installments_payments
