6b7cd57c01f7dc9c669ee198a7ef3980042b7736
aplandaeta <antonia.landaeta@ug.uchile.cl>
2025-06-09 17:39:28 -0400

    Update create_table_credit_card_balance.sql

--- query/create_table_credit_card_balance.sql
+++ query/create_table_credit_card_balance.sql
@@ -2,33 +2,33 @@
 
 DROP TABLE IF EXISTS credit_card_balance; 
 
-CREATE TABLE credit_card_balance (
-    SK_ID_PREV INT, -- ID único de la solicitud previa
-    SK_ID_CURR INT, -- ID único del cliente
-    MONTHS_BALANCE INT, -- Meses relativos al balance (0 = mes actual)
-    AMT_BALANCE DECIMAL(15, 2), -- Balance del crédito
-    AMT_CREDIT_LIMIT_ACTUAL DECIMAL(15, 2), -- Límite de crédito actual
-    AMT_DRAWINGS_ATM_CURRENT DECIMAL(15, 2), -- Monto de retiros en cajeros automáticos
-    AMT_DRAWINGS_CURRENT DECIMAL(15, 2), -- Monto total de retiros
-    AMT_DRAWINGS_OTHER_CURRENT DECIMAL(15, 2), -- Monto de otros retiros
-    AMT_DRAWINGS_POS_CURRENT DECIMAL(15, 2), -- Monto de retiros en puntos de venta
-    AMT_INST_MIN_REGULARITY DECIMAL(15, 2), -- Pago mínimo regular
-    AMT_PAYMENT_CURRENT DECIMAL(15, 2), -- Pago actual
-    AMT_PAYMENT_TOTAL_CURRENT DECIMAL(15, 2), -- Pago total actual
-    AMT_RECEIVABLE_PRINCIPAL DECIMAL(15, 2), -- Principal pendiente
-    AMT_RECIVABLE DECIMAL(15, 2), -- Monto pendiente
-    AMT_TOTAL_RECEIVABLE DECIMAL(15, 2), -- Total pendiente
-    CNT_DRAWINGS_ATM_CURRENT INT, -- Número de retiros en cajeros automáticos (**en csv viene como float**)
-    CNT_DRAWINGS_CURRENT INT, -- Número total de retiros
-    CNT_DRAWINGS_OTHER_CURRENT INT, -- Número de otros retiros
-    CNT_DRAWINGS_POS_CURRENT INT, -- Número de retiros en puntos de venta
-    CNT_INSTALMENT_MATURE_CUM INT, -- Número acumulado de cuotas vencidas
-    NAME_CONTRACT_STATUS VARCHAR(50), -- Estado del contrato (por ejemplo, 'Active', 'Completed')
-    SK_DPD INT, -- Días de retraso en el pago
-    SK_DPD_DEF INT -- Días de retraso en el pago con incumplimiento
+CREATE TABLE IF NOT EXISTS proyecto.credit_card_balance
+(
+    sk_id_prev integer NOT NULL,
+    sk_id_curr integer NOT NULL,
+    months_balance integer NOT NULL,
+    amt_balance numeric(15,2),
+    amt_credit_limit_actual numeric(15,2),
+    amt_drawings_atm_current numeric(15,2),
+    amt_drawings_current numeric(15,2),
+    amt_drawings_other_current numeric(15,2),
+    amt_drawings_pos_current numeric(15,2),
+    amt_inst_min_regularity numeric(15,2),
+    amt_payment_current numeric(15,2),
+    amt_payment_total_current numeric(15,2),
+    amt_receivable_principal numeric(15,2),
+    amt_recivable numeric(15,2),
+    amt_total_receivable numeric(15,2),
+    cnt_drawings_atm_current double precision,
+    cnt_drawings_current double precision,
+    cnt_drawings_other_current double precision,
+    cnt_drawings_pos_current double precision,
+    cnt_instalment_mature_cum double precision,
+    name_contract_status character varying(50) COLLATE pg_catalog."default",
+    sk_dpd integer,
+    sk_dpd_def integer,
     CONSTRAINT pk_cc_balance PRIMARY KEY (sk_id_prev, sk_id_curr, months_balance)
-
-);
+)
 
 SET ROLE postgres;
 COPY credit_card_balance
